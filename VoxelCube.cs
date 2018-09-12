8d2d41506267e94f1e83e5723a46dea0fce4ca36
KLLRBKLLD <35475608+marcnholley@users.noreply.github.com>
2018-09-11 21:24:06 -0700

    Add files via upload

---
+++ VoxelCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        for (int i = 0; i < 8; i++)
+            for (int j = 0; j < 8; j++)
+                for (int k = 0; k < 8; k++)
+                    VoxelTools.MakeCube(i + 150, j + 20, k + 150);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+            VoxelTools.MakeAllCubesFall();
+    }
+}
